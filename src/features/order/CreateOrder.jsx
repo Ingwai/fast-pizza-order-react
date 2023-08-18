d32d52c824e9b3729b04e1da01eacb8002b6cfda
Ingwai <ingwai@wp.pl>
2023-08-18 16:50:23 +0200

    fetching menu, orders, loadingindicators, actions and error handling

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
+import { createOrder } from '../../services/apiRestaurant';
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = str => /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(str);
@@ -28,14 +30,19 @@ const fakeCart = [
 ];
 
 function CreateOrder() {
+	const navigation = useNavigation();
+	const isSubmiting = navigation.state === 'submitting';
+	// używamy tego hooka aby zwrócić ewentualne błędy zanim wyślemy zamówienie
+	const formErrors = useActionData();
 	// const [withPriority, setWithPriority] = useState(false);
 	const cart = fakeCart;
 
 	return (
 		<div>
 			<h2>Ready to order? Lets go!</h2>
-
-			<form>
+			{/* w FORM działają tylko metody POST, PUT, DELETE nie działa GET */}
+			{/* <Form method='POST' action='/order/new'> */}
+			<Form method='POST'>
 				<div>
 					<label>First Name</label>
 					<input type='text' name='customer' required />
@@ -46,6 +53,7 @@ function CreateOrder() {
 					<div>
 						<input type='tel' name='phone' required />
 					</div>
+					{formErrors?.phone && <p>{formErrors.phone}</p>}
 				</div>
 
 				<div>
@@ -63,15 +71,38 @@ function CreateOrder() {
 						// value={withPriority}
 						// onChange={(e) => setWithPriority(e.target.checked)}
 					/>
-					<label htmlFor='priority'>Want to yo give your order priority?</label>
+					<label htmlFor='priority'>Want to you give your order priority?</label>
 				</div>
 
 				<div>
-					<button>Order now</button>
+					<input type='hidden' name='cart' value={JSON.stringify(cart)} />
+					<button disabled={isSubmiting}>{isSubmiting ? 'Placing order...' : 'Order now'}</button>
 				</div>
-			</form>
+			</Form>
 		</div>
 	);
+}
+
+export async function action({ request }) {
+	const formData = await request.formData();
+	const data = Object.fromEntries(formData);
+
+	const order = {
+		...data,
+		cart: JSON.parse(data.cart),
+		priority: data.priority === 'on',
+	};
+
+	const errors = {};
+	if (!isValidPhone(order.phone))
+		errors.phone = 'Please give us your correct phone number. We might need it to contact you.';
+
+	if (Object.keys(errors).length > 0) return errors;
+
+	// if ok, we crete new order and redirect
+	const newOrder = await createOrder(order);
+
+	return redirect(`/order/${newOrder.id}`);
 }
 
 export default CreateOrder;
